376d9e0d17f874e8cd7c02d924e6a8d77c2539eb
Jenny <yenny501ss@gmail.com>
2025-07-05 18:19:04 -0500

    Se realiza con un record la conversion de moneda

--- src/Modelo/Conversion.java
+++ src/Modelo/Conversion.java
@@ -1,4 +1,11 @@
 package Modelo;
 
-public class Conversion {
+public record Conversion(String origen,
+                         String destino,
+                         double monto,
+                         double resultado) {
+    @Override
+    public String toString(){
+        return "%.2f %s => %.2f %s".formatted(monto,origen,resultado,destino);
+    }
 }
