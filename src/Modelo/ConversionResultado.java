72a7b3023dfc028d48a0dd561e46fb006e13efcd
Jenny <yenny501ss@gmail.com>
2025-07-05 02:30:48 -0500

    leyendo los datos reales desde la API y convirtiendolos a obj Java

---
+++ src/Modelo/ConversionResultado.java
@@ -0,0 +1,15 @@
+package Modelo;
+import java.util.Map;
+
+public class ConversionResultado {
+    private String base_code;
+    private Map<String, Double> conversion_rates;
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public Map<String, Double> getConversion_rates() {
+        return conversion_rates;
+    }
+}
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,12 +1,21 @@
 package Principal;
 
+import Modelo.ConversionResultado;
 import Servicio.ConsultaMoneda;
+import com.google.gson.Gson;
 
 public class Principal {
     public static void main(String[] args) {
         ConsultaMoneda consulta = new ConsultaMoneda();
         String json =consulta.obtenerDatos("USD");
-        System.out.println(json);
 
+        //Convertir Json en objeto Java
+        Gson gson = new Gson();
+        ConversionResultado resultado = gson.fromJson(json, ConversionResultado.class);
+
+        //Mostrar algunas clases de cambio
+        System.out.println("Moneda Base: " + resultado.getBase_code());
+        System.out.println("USD a PEN: " + resultado.getConversion_rates().get("PEN"));
+        System.out.println("USD a EUR: " + resultado.getConversion_rates().get("EUR"));
     }
 }
